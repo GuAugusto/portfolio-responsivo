eebd3e5cb0a3c40de53d75f1d548f97049d83c4f
GuAugusto <gustavoaugusto77@hotmail.com>
2021-10-23 01:36:32 -0300

    Seção Habilidades

--- static/script.js
+++ static/script.js
@@ -8,4 +8,18 @@ menuMobile.addEventListener('click', () => {
         ? menuMobile.classList.replace("bi-list", "bi-x")
         : menuMobile.classList.replace("bi-x", "bi-list");
         body.classList.toggle("menu-nav-active");
-})
+});
+
+
+/** Vai fechar o menu quando clicar em algum item e mudar o icone para list */
+
+const navItem = document.querySelectorAll(".nav-item");
+
+navItem.forEach((item) => {
+  item.addEventListener("click", () => {
+    if (body.classList.contains("menu-nav-active")) {
+      body.classList.remove("menu-nav-active");
+      menuMobile.classList.replace("bi-x", "bi-list");
+    }
+  });
+});
